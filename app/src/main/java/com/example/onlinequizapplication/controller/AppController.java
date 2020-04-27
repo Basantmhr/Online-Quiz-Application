119d7985a4c6ba55c46b7683f7aaaed86653edc3
Basantmhr <Basantmhr88@gmail.com>
2020-04-27 21:50:37 +0530

    Created Volley Singleton in AppController to handle RequestQueue

--- app/src/main/java/com/example/onlinequizapplication/controller/AppController.java
+++ app/src/main/java/com/example/onlinequizapplication/controller/AppController.java
@@ -2,5 +2,30 @@ package com.example.onlinequizapplication.controller;
 
 import android.app.Application;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class AppController extends Application {
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
+    public static synchronized AppController getInstance()
+    {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance=this;
+    }
+    public RequestQueue getRequestQueue()
+    {
+        if(mRequestQueue==null)
+            mRequestQueue= Volley.newRequestQueue(getApplicationContext());
+        return mRequestQueue;
+    }
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
 }
