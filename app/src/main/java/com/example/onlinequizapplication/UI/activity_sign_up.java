ca9d0cfc89869fe7c3cb8bde6a08db4bdc74fde8
Basantmhr <Basantmhr88@gmail.com>
2020-04-29 13:22:53 +0530

    Some UI changes

--- app/src/main/java/com/example/onlinequizapplication/UI/activity_sign_up.java
+++ app/src/main/java/com/example/onlinequizapplication/UI/activity_sign_up.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.onlinequizapplication.R;
@@ -13,10 +14,14 @@ public class activity_sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
